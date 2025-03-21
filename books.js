280fb7139bb0256bf0cb06b320ae4658fdc390ba
Chris <cldarling@gmail.com>
2025-03-21 14:18:58 -0400

    building e-commerce app complete, fixed ratings not showing

--- books.js
+++ books.js
@@ -1,53 +1,58 @@
-
 function renderBooks(filter) {
-  const booksWrapper = document.querySelector(".books")
+  const booksWrapper = document.querySelector(".books");
   const books = getBooks();
 
-  
   if (filter === "LOW_TO_HIGH") {
-    console.log(filter)
+    console.log(filter);
     books.sort((a, b) => a.originalPrice - b.originalPrice);
-  }
-  else if (filter === "HIGH_TO_LOW") {
+  } else if (filter === "HIGH_TO_LOW") {
     books.sort((a, b) => b.originalPrice - a.originalPrice);
-  }
-  else if (filter === "RATING") {
+  } else if (filter === "RATING") {
     books.sort((a, b) => b.rating - a.rating);
   }
 
-  const booksHtml = books.map(book => {
-   return `<div class="book">
+  const booksHtml = books
+    .map((book) => {
+      return `<div class="book">
     <figure class="book__img--wrapper">
       <img class="book__img" src="${book.url}" alt="" />
     </figure>
     <div class="book__title">
-    ${books[0].title}
+    ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+    ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
       <span>$${book.originalPrice.toFixed(2)}</span> 
     </div>
   </div>`;
-  })
-  .join("");
+    })
+    .join("");
 
   booksWrapper.innerHTML = booksHtml;
- 
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fas fa-star"></i>\n';
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+  }
+  return ratingHTML;
 }
 
 function filterBooks(event) {
-renderBooks(event.target.value)
+  renderBooks(event.target.value);
 }
 
 setTimeout(() => {
   renderBooks();
 });
+
+
 
 
 // FAKE DATA
@@ -56,7 +61,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
